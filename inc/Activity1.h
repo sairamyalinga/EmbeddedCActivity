91e6c0a51cdc8e3d03c7840b11239d1e6168f545
sairamyalinga <67336902+sairamyalinga@users.noreply.github.com>
2021-04-28 00:25:36 +0530

    Update Activity1.h

--- inc/Activity1.h
+++ inc/Activity1.h
@@ -1,7 +1,8 @@
 #ifndef _ACTIVITY_1_H_
 #define _ACTIVITY_1_H_
 
-#define SEATED ((!(PIND&(1<<PD0)))&&(!(PIND&(1<<PD3))));
+#define SEATED ((!(PIND&(1<<PD0)))&&(!(PIND&(1<<PD3))))
+
 void defining_ports(void);
 
 
