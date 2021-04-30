16c26e02facdb43afdc29fa3cf4cd7cc6392b83e
sairamyalinga <67336902+sairamyalinga@users.noreply.github.com>
2021-04-30 17:37:10 +0530

    Update Activity3.c

--- src/Activity3.c
+++ src/Activity3.c
@@ -5,7 +5,7 @@
 
 
 
-void PWM_PERIPHERALS(void){
+void pwmperipherals(void){
     TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11);
     TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10); // 64 prescaling
     DDRB|=(1<<PB1);
