ee4eb37b6c9c2ad60e650f90e5df0b6448230935
sairamyalinga <67336902+sairamyalinga@users.noreply.github.com>
2021-04-30 16:41:41 +0530

    Update Activity1.h

--- inc/Activity1.h
+++ inc/Activity1.h
@@ -1,8 +1,9 @@
 #ifndef _ACTIVITY_1_H_
 #define _ACTIVITY_1_H_
 
-#define SEATED ((!(PIND&(1<<PD0)))&&(!(PIND&(1<<PD3))))
-
+#define SEATED_N_HEATERON ((!(PIND&(1<<PD0)))&&(!(PIND&(1<<PD3))))
+#define LED_ON (PORTB |=(1<<PB1))
+#define LED_OFF (PORTB &=~(1<<PB1))
 void defining_ports(void);
 
 
