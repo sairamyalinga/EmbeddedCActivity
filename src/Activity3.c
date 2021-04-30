02c6aa207d4256ca9890ebaa45b80e2126303f58
sairamyalinga <67336902+sairamyalinga@users.noreply.github.com>
2021-04-30 17:06:08 +0530

    Update Activity3.c

--- src/Activity3.c
+++ src/Activity3.c
@@ -8,14 +8,13 @@ void PWM_PERIPHERALS(){
     DDRB|=(1<<PB1);
 
 }
-char temp_output (uint16_t ADCvalue)
-{
+char temp_output (uint16_t ADCvalue){
     
     char temperature;
 
     if((ADCvalue>=0) && (ADCvalue<=200)){
 
-        OCR1A = 205; //20% duty cycle (0.2*(max(1024)))
+        OCR1A = 205; //20% 
         temperature = 20;
         _delay_ms(30);
     }
